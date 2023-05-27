20700e41df63c2080dfe8e2e59ad339b2340929a
4thabang <puresneakers1@gmail.com>
2023-05-27 17:48:55 +0100

    refactor: add \<CR\> for command remaps

--- .config/nvim/lua/deploi/remap.lua
+++ .config/nvim/lua/deploi/remap.lua
@@ -7,8 +7,8 @@ vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
 vim.keymap.set("n", "<S-Tab>", vim.cmd.bprev)
 vim.keymap.set("n", "<C-s>", "ysw") -- Vim Surround Single Word Wrap
-vim.keymap.set("n", "<Leader>+", "<cmd> vertical resize +5")
-vim.keymap.set("n", "<Leader>-", "<cmd>vertical resize -5")
+vim.keymap.set("n", "<Leader>+", "<cmd> vertical resize +5<CR>")
+vim.keymap.set("n", "<Leader>-", "<cmd>vertical resize -5<CR>")
 
 vim.keymap.set("n", "<Leader>k", vim.cmd.Autoformat)
 vim.keymap.set("n", "<Leader>d", vim.cmd.bd)
@@ -24,8 +24,8 @@ vim.keymap.set("n", "<Leader>q", vim.cmd.q)
 
 vim.keymap.set("n", "gb", vim.cmd.BufferLinePick, { silent = true })
 
-vim.keymap.set("n", "<Leader>gc", "<cmd>Git commit")
-vim.keymap.set("n", "<Leader>gp", "<cmd>Git push")
+vim.keymap.set("n", "<Leader>gc", "<cmd>Git commit<CR>")
+vim.keymap.set("n", "<Leader>gp", "<cmd>Git push<CR>")
 vim.keymap.set("n", "<Leader>G", vim.cmd.G)
 
 vim.keymap.set("n", "<Leader>n", vim.cmd.NvimTreeToggle)
