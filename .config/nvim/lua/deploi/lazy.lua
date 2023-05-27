edb38ad24bf93f03015bb70748527e7cc99e5ac6
4thabang <puresneakers1@gmail.com>
2023-05-27 17:38:47 +0100

    chore: remove redundant comments

--- .config/nvim/lua/deploi/lazy.lua
+++ .config/nvim/lua/deploi/lazy.lua
@@ -75,19 +75,19 @@ local plugins = {
   branch = 'v2.x',
   dependencies = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
+      {'neovim/nvim-lspconfig'},
+      {
         'williamboman/mason.nvim',
         build = function()
           pcall(vim.cmd, 'MasonUpdate')
         end,
       },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      {'williamboman/mason-lspconfig.nvim'},
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
     }
   },
 
