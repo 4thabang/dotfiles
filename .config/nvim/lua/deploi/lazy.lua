6bd8085186fa9bad08659943da557e02a587c228
4thabang <puresneakers1@gmail.com>
2023-08-22 15:14:22 +0100

    deps: remove `error-lens`

--- .config/nvim/lua/deploi/lazy.lua
+++ .config/nvim/lua/deploi/lazy.lua
@@ -27,87 +27,79 @@ local plugins = {
     opts = {}
   },
 
--- Themes/Colorscheme
+  -- Themes/Colorscheme
   "nvim-lualine/lualine.nvim",
   { "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons"
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end
-  },
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons"
+},
+{
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end
+},
 
 -- Navigation
-  "nvim-lua/popup.nvim",
-  "nvim-lua/plenary.nvim",
-  "nvim-telescope/telescope.nvim",
-  "nvim-telescope/telescope-fzy-native.nvim",
-  "kyazdani42/nvim-tree.lua",
+"nvim-lua/popup.nvim",
+"nvim-lua/plenary.nvim",
+"nvim-telescope/telescope.nvim",
+"nvim-telescope/telescope-fzy-native.nvim",
+"kyazdani42/nvim-tree.lua",
 
 -- Languages
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  "nvim-treesitter/playground",
-  { "folke/neodev.nvim", opts = {}},
-  {
-    "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-  },
-  { "hrsh7th/cmp-path", branch = "main" },
-  { "hrsh7th/cmp-buffer", branch = "main" },
-  "hrsh7th/vim-vsnip",
-  {
-    "chikko80/error-lens.nvim",
-    event = "BufRead",
-    dependencies = {
-        "nvim-telescope/telescope.nvim"
-    },
-    opts = {},
-  },
-  {
+{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+"nvim-treesitter/playground",
+{ "folke/neodev.nvim", opts = {}},
+{
+  "windwp/nvim-autopairs",
+  config = function() require("nvim-autopairs").setup {} end
+},
+{ "hrsh7th/cmp-path", branch = "main" },
+{ "hrsh7th/cmp-buffer", branch = "main" },
+"hrsh7th/vim-vsnip",
+{
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v2.x',
   dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'},
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    {
+      'williamboman/mason.nvim',
+      build = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'},
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'L3MON4D3/LuaSnip'},
-    }
-  },
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+},
 
 -- Debugging
-  "mfussenegger/nvim-dap",
-  "leoluz/nvim-dap-go",
-  "rcarriga/nvim-dap-ui",
-  "theHamsta/nvim-dap-virtual-text",
-  "nvim-telescope/telescope-dap.nvim",
-  { "mxsdev/nvim-dap-vscode-js", dependencies = {"mfussenegger/nvim-dap"} },
+"mfussenegger/nvim-dap",
+"leoluz/nvim-dap-go",
+"rcarriga/nvim-dap-ui",
+"theHamsta/nvim-dap-virtual-text",
+"nvim-telescope/telescope-dap.nvim",
+{ "mxsdev/nvim-dap-vscode-js", dependencies = {"mfussenegger/nvim-dap"} },
 
 -- JavaScript, TypeScript, React
-  "jose-elias-alvarez/null-ls.nvim",
-  "MunifTanjim/prettier.nvim",
+"jose-elias-alvarez/null-ls.nvim",
+"MunifTanjim/prettier.nvim",
 
 -- Misc.
-  "mbbill/undotree",
-  { "mg979/vim-visual-multi", branch = "master" },
-  "onsails/lspkind-nvim",
-  "andweeb/presence.nvim",
+"mbbill/undotree",
+{ "mg979/vim-visual-multi", branch = "master" },
+"onsails/lspkind-nvim",
+"andweeb/presence.nvim",
 }
 
 require("lazy").setup(plugins, opts)
